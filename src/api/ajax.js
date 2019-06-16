c2cd4e7549dbd4842b89481c86395d1c537dbfc8
liuyi <liuyi@sina.com>
2019-06-16 19:55:03 +0800

    ajax请求封装

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,20 +1,44 @@
 import axios from 'axios'
-/* 
- */
-export default function ajax(url, data = {}, method = 'GET') {
-    if (method === 'GET') {
-        axios.get(url, {
-            params: data //指定quey参数
-        })
 
-    } else {
-        axios.post(url, data)
-    }
+ 
+export default function ajax(url, data = {}, method = 'GET') {
+    return new Promise ((resolve,reject)=>{
+        let promise
+        if (method === 'GET') {
+           
+         promise =axios.get(url, {
+                params: data //指定quey参数
+            })
+    
+        } else {
+             promise = axios.post(url, data)
+        }
+
+        promise.then(
+            response =>{
+                resolve(response.data)
+            },
+            error=>{
+                alert('请求错误：' + error.message)
+            }
+        )
+    })
+    
 }
-async function login() {
-    try {
-        const response = await ajax('/login', { username: 'admin', password: 'admin' }, 'POST')
-    } catch (error) {
-        alert('请求错误：' + error.message)
-    }
-}
+// async function login() {
+//    /*  try {
+//         const response = await ajax('/login', { username: 'admin', password: 'admin' }, 'POST')
+//     } catch (error) {
+//         alert('请求错误：' + error.message)
+//     } */
+//     const resule = await ajax('/login',{
+//         username:'admin',
+//         password:'admin'
+//     },'POST')
+    
+//     if (resule.status===0) {
+        
+//     } else {
+        
+//     }
+// }
---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+
+import ajax from "./ajax";
+// const BASE = 'http://localhost:3001'
+const BASE = ''
+
+// 1. 登陆
+
+export const reqLogin = (username, password) => ajax(BASE + '/login', {username, password}, 'POST')
+
+// 添加用户
+export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST')
+
+
+
+
+
