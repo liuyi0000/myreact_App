9c2d1fec2a7b39f972fb0d4c2dbeadbe24769cb8
liuyi <liuyi@sina.com>
2019-06-17 22:54:58 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './api'
-=======
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import'./api'
->>>>>>> 0c58405daccc6877f7ca9220bd1ced0c9bbb63d1
 
 import App from './App' // 自定义的模块引入必须以.开头
 import memoryUtils from './utils/memoryUtils';
