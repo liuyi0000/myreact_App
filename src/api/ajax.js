f0df83b41a87d2a571301a5498d80a390d036c75
liuyi <liuyi@sina.com>
2019-06-17 23:14:05 +0800

    1

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -36,22 +36,4 @@ export default function ajax(url,data={},method="GET"){
    
 }
 
-async function login (){
-    // try{
-    //     const response = await  ajax('/login',{uaername:'',password:''},'POST')
-    // }catch(error){
-    //   //处理请求异常
-    //    alert('出错')
-    // }
-   const result = await ajax('/login',{
-       username:'admin',
-       password:'admin'
-    },'POST')
-    // const result = response.data //必须从response取出data属性值才是响应数据
-    if(result.status===0){
-        
-    }else{
-
-    }
-}
 
